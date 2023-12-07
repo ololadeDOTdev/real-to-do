3390628ae319ae70bba2341e72c81cf62ef6b755
Ayoola Ololade <ayoola.ololade2014@gmail.com>
2023-12-07 14:29:32 +0100

    background

--- est/src/App.vue
+++ est/src/App.vue
@@ -39,6 +39,9 @@ const addTodo = () => {
     editable: false,
     createdAt: new Date().getTime(),
   });
+
+  input_content.value = "";
+  input_category.value = null;
 };
 
 const removeTodo = (todo) => {
